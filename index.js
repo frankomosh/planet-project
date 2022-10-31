5fab2c4ff0afc787d2f1f441406020cecef7162b
Frank-197 <frankomosh197@gmail.com>
2022-10-31 10:01:31 +0300

    finding all habitable planets

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const parse = require('csv-parse');
-const fs =require('fs');
+const { parse } = require('csv-parse');
+const fs = require('fs');
 
+const habitablePlanets = [];
 
-const results=[];
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
 
 fs.createReadStream('kepler_data.csv')
-  .on('data', (data)=>{
-    results.push(data);
-
+  .pipe(parse({
+    comment: '#',
+    columns: true,
+  }))
+  .on('data', (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
-  .on('error', (err)=>{
-    console.log(err)
+  .on('error', (err) => {
+    console.log(err);
   })
-  .on('end', ()=>{
-    console.log(results);
-    console.log('done');
+  .on('end', () => {
+    console.log(habitablePlanets.map((planet) => {
+      return planet['kepler_name'];
+    }));
+    console.log(`${habitablePlanets.length} habitable planets found!`);
   });
-
-// parse();
